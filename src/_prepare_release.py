c70bc7fba4531f39929bdd7440d195da8fc69523
KevinVG207 <kevin2000.van.geemen@gmail.com>
2023-10-01 00:17:40 +0200

    More UI

--- src/_prepare_release.py
+++ src/_prepare_release.py
@@ -1,9 +1,9 @@
 import intermediate
-import _fill_duplicates
+# import _fill_duplicates
 import autofill_mdb
 
 def main():
-    _fill_duplicates.main()
+    # _fill_duplicates.main()
     autofill_mdb.main()
     intermediate.mdb_from_intermediate()
     intermediate.assets_from_intermediate()
