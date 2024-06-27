e8948d50bb10244546e12649dc0c0f4febeb1a73
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-06-27 14:19:10 +0200

    Version bump.

--- src/version.py
+++ src/version.py
@@ -1,6 +1,6 @@
 # In case more metadata is added in the future, we need to keep track of the version of the intermediate files.
 
-VERSION = (24, 5, 23, 0)
+VERSION = (24, 6, 27, 0)
 
 def version_to_string(version):
     return "v" + ".".join(str(v) for v in version)
