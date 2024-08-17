3c6bbb7c184041da1c66090328f4fdb4958af289
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-08-17 16:48:32 +0200

    Hachimi converter.

--- src/util.py
+++ src/util.py
@@ -176,6 +176,7 @@ def load_json(path):
 
 
 def save_json(path, data):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w", encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
